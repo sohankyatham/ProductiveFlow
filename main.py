33bcea58f2bba5092a5fbba2faaee53432dc9391
Sohan Kyatham <45246932+sohankyatham@users.noreply.github.com>
2022-01-16 16:30:44 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# ProductiveFlow - A Productivity App
+# By: Sohan Kyatham
+
+# Imports
+from tkinter import *
+
+
+
+# Create Screen
+root = Tk()
+root.geometry("600x600")
+root.title("ProductiveFlow")
+
+
+
+# Initialize Screen
+root.mainloop()
