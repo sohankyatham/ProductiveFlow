ab4bbc84a20e77303c7c9dd87062737688c52e55
Sohan Kyatham <45246932+sohankyatham@users.noreply.github.com>
2022-01-19 07:30:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 # ProductiveFlow - A Productivity App
 # By: Sohan Kyatham
 
-# Figure out which tab the user is on
-#https://stackoverflow.com/questions/14000944/finding-the-currently-selected-tab-of-ttk-notebook
-
-
-
-# Disable Menus in Tkinter
-#https://www.youtube.com/watch?v=s1WDk9-jJ6A
-#https://stackoverflow.com/questions/23442792/tkinter-enable-disable-menu
-
-
-
 
 # Imports
 from tkinter import *
 from tkinter import ttk
-#import calendar?
+from tkinter import messagebox
 from tkinter import filedialog
 import tkinter.font as tkfont
 
@@ -27,29 +16,71 @@ root.geometry("600x600")
 root.title("ProductiveFlow")
 
 
+# Global OpenFileName - used for finding name/status of opened file and using it later in code for functions such as saving a file and etc
+global OpenFileName
+OpenFileName = False
+
+
+
 '''
 Functions for MenuBar Options
 '''
 
 
+
 # FileMenu: New File Function
-def NewFileFunc():
-    print("New File Created")
+def NewFileFunc(*args):
+    TextBoxForNotes.delete("1.0", END) 
+root.bind('<Control-Key-n>', NewFileFunc)
 
 
 # FileMenu: Open File Function
-def OpenFileFunc():
-    print("Opening a file...")
+def OpenFileFunc(*args):
+    # Open File Dialog Asking User Which File They Want to Open
+    FilePath = filedialog.askopenfilename(initialdir="C:/gui/", title="Open a File", filetypes=(("All Files", "*.*"), ("Text Files", "*.txt"), ("HTML Files", "*.html"), ("CSS Files", "*.css"),("JavaScript Files", "*.js"), ("Python Files", "*.py"), ("All Files", "*.*")))
+    print(FilePath)
+    
+    # FilePath is the name of the file and has details corresponding to the file
+   
+    # Find the Name/Status of the File & use it later in code for other functions such as saving files and saving them as
+    if FilePath:
+        global OpenFileName
+        OpenFileName = FilePath
+    
+    TextBoxForNotes.delete("1.0", END)
+
+    # Open File and Insert File Content into Editor
+    FilePath = open(FilePath, 'r')
+    FileContent = FilePath.read()
+    TextBoxForNotes.insert(END, FileContent)
+    FilePath.close()
+root.bind('<Control-Key-o>', OpenFileFunc)
 
 
 # FileMenu: Save File Function
-def SaveFileFunc():
-    print("Saving file...")
+def SaveFileFunc(*args):
+    global OpenFileStatusName
+
+    # If File has been opened then save
+    if OpenFileName:
+        FilePath = open(OpenFileName, "w")
+        FilePath.write(TextBoxForNotes.get(1.0, END))
+        FilePath.close()
+    # If the file does not exist, then save this file as a file
+    else:
+        SaveAsFunc()
+root.bind('<Control-Key-s>', SaveFileFunc)
 
 
 # FileMenu: Save As Function
-def SaveAsFunc():
-    print("Saving file as...")
+def SaveAsFunc(*args):
+    FilePath = filedialog.asksaveasfilename(defaultextension=".*", initialdir="C:/gui/", title="Save File As", filetypes=(("All Files", "*.*"), ("Text Files", "*.txt"), ("HTML Files", "*.html"), ("CSS Files", "*.css"), ("JavaScript Files", "*.js"), ("Python Files", "*.py")))
+    if FilePath:
+        # Save the File
+        FilePath = open(FilePath, "w")
+        FilePath.write(TextBoxForNotes.get(1.0, END))
+        FilePath.close()
+root.bind('<Control-Shift-S>', SaveAsFunc)
 
 
 # FileMenu: Auto Save Function
@@ -75,6 +106,7 @@ def AboutScreenFunc():
 
     # Mainloop for AboutScreen
     AboutScreen.mainloop()
+
 
 
 '''
@@ -161,8 +193,52 @@ To-Do List Section
 
 
 # To-Do List Frame - A Frame to Place To-do List Program
-ToDoListFrame = Frame(TabControl, width="590", height="590", bg="blue")
+ToDoListFrame = Frame(TabControl, width="590", height="590")
 ToDoListFrame.pack(fill="both", expand=1)
+
+
+# TitleLabel - Displays a Title on the Screen called "To-Do List"
+TitleLabel = Label(ToDoListFrame, text="To-Do List", font=("Arial", 25, 'bold'))
+TitleLabel.pack(side="top", fill=BOTH)
+
+
+'''Add Tasks Sections'''
+
+# AddTaskFrame - A Frame for Storing all the Widgets Regarding Add Tasks Functions
+AddTaskFrame = Frame(ToDoListFrame, width=250, height=500, bg="lightblue")
+AddTaskFrame.pack(side=LEFT, anchor=NE, pady=20)
+
+
+# EnterTaskLabel - Displays a Label on the Screen called "Enter Your Task"
+EnterTaskLabel = Label(AddTaskFrame, text="Enter Your Task:", font=("Arial", 18))
+EnterTaskLabel.pack()
+
+
+# EnterTaskEntry - Displays an Entry Widget Where User Can Enter Their Task
+EnterTaskEntry = Entry(AddTaskFrame, font=("Arial", 12))
+EnterTaskEntry.pack(pady=5)
+
+
+# AddTaskButton - Displays a Button called "Add Task" Which Adds a Task into a list of Tasks 
+AddTaskButton = Button(AddTaskFrame, text="Add Task", font=("Arial", 12), command=None)
+AddTaskButton.pack(pady=5)
+
+
+'''Tasks Section'''
+
+# TasksFrame - A Frame for Storing all the Widgets Regarding Task Functions
+TasksFrame = Frame(ToDoListFrame, width=350, height=500, bg="yellow")
+TasksFrame.pack(side=RIGHT, anchor=NW)
+
+
+# TasksLabel - Displays a Label on the Screen called "Tasks:"
+TasksLabel = Label(TasksFrame, text="Tasks:", font=("Arial", 18))
+TasksLabel.pack()
+
+
+# ListBoxForTDList - Displays a Listbox Widget on the Screen or Showing the Tasks That the User Has Entered
+ListBoxForTDList = Listbox(TasksFrame, width=55, height=30)
+ListBoxForTDList.pack()
 
 
 # Add To-Do List Frame to Tab Control
@@ -177,7 +253,7 @@ Notes Section
 
 
 # Notes Frame - A Frame to Place Notes Program
-NotesFrame = Frame(TabControl, width="590", height="590", bg="#606060")
+NotesFrame = Frame(TabControl, width="590", height="590")
 NotesFrame.pack(fill="both", expand=1)
 
 
@@ -197,20 +273,20 @@ HorizontalScrollbar = Scrollbar(NotesFrame, orient="horizontal")
 HorizontalScrollbar.pack(side=BOTTOM, fill=X)
 
 
-# TextBox - Place for Writing Notes/Text
-TextBox = Text(NotesFrame, width=500, height=500, font=("DejaVu Sans Mono", 16), selectbackground="Skyblue", selectforeground="black", undo=True, wrap="none", yscrollcommand=VerticalScrollbar.set, xscrollcommand=HorizontalScrollbar.set)
-TextBox.pack()
+# TextBoxForNotes - Place for Writing Notes/Text
+TextBoxForNotes = Text(NotesFrame, width=500, height=500, font=("DejaVu Sans Mono", 16), selectbackground="Skyblue", selectforeground="black", undo=True, wrap="none", yscrollcommand=VerticalScrollbar.set, xscrollcommand=HorizontalScrollbar.set)
+TextBoxForNotes.pack()
 
 
 # Set Default Tab Size (4 Spaces) for Text Box 
-font = tkfont.Font(font=TextBox['font'])
+font = tkfont.Font(font=TextBoxForNotes['font'])
 TabSize = font.measure("    ") # 4 Spaces
-TextBox.config(tabs=TabSize)
+TextBoxForNotes.config(tabs=TabSize)
 
 
 # Configure the Vertical and Horizontal Scrollbar
-VerticalScrollbar.config(command=TextBox.yview)
-HorizontalScrollbar.config(command=TextBox.xview)
+VerticalScrollbar.config(command=TextBoxForNotes.yview)
+HorizontalScrollbar.config(command=TextBoxForNotes.xview)
 
 
 # Add Notes Frame to Tab Control
