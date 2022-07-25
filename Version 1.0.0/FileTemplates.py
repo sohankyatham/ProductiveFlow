9e8f226c49a75cadc97ef2fede0586cb6058b191
Sohan Kyatham <45246932+sohankyatham@users.noreply.github.com>
2022-07-25 17:51:30 -0400

    Add files via upload

---
+++ Version 1.0.0/FileTemplates.py
@@ -0,0 +1,58 @@
+# This file is for storing File Templates that can be used in the code editor
+
+HTML_FileTemplateCode = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Untitled Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+"""
+
+
+
+PHP_FileTemplateCode = """
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Untitled</title>
+</head>
+<body>
+    
+</body>
+</html>
+"""
+
+
+
+# Python Start File Template
+Python_FileTemplateCode = """
+def main(args):
+    return 0
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
+"""
+
+
+
+# Java Start File Template
+Java_FileTemplateCode = """
+public class untitled {
+	
+	public static void main (String[] args) {
+		
+	}
+}
+"""
